7dcd65c335be25ec614a30456783db83359cd447
Tucker Sheram <tgsher9329@outlook.com>
2021-04-13 19:45:58 -0400

    adding pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+} from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import YoutubeDetail from './components/YoutubeDetail';
+import InstagramDetail from './components/InstagramDetail';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Switch>
+          <Route path="/" exact >
+            <Dashboard />
+          </Route>
+          <Route path="/youtubeDetail" >
+            <YoutubeDetail />
+          </Route>
+          <Route path="/instagramDetail" >
+            <InstagramDetail />
+          </Route>
+        </Switch>
+      
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Sales from './Sales'
+
+export default function Dashboard() {
+    return (
+        <div>
+            <Sales />
+        </div>
+    )
+}
